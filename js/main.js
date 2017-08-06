9b58e7f84b9cc73a4e8d5353ecf553bbcd830950
“juliantreweeke” <juliantreweeke@gmail.com>
2017-08-06 21:30:29 +1000

    half way done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,41 @@
-alert('working');
+
+
+$(document).ready(function(){
+
+  $('.drm-link').click(function(){
+    $('.drm-modal').modal('show');
+  });
+  $(".drm-modal").modal({
+  closable: true
+  });
+
+  $('.fraga-link').click(function(){
+    $('.fraga-modal').modal('show');
+  });
+  $(".fraga-modal").modal({
+  closable: true
+  });
+
+  $('.soundslap-link').click(function(){
+    $('.soundslap-modal').modal('show');
+  });
+  $(".soundslap-modal").modal({
+  closable: true
+  });
+
+  $('.tictactoe-link').click(function(){
+    $('.tictactoe-modal').modal('show');
+  });
+  $(".tictactoe-modal").modal({
+  closable: true
+  });
+
+
+
+
+
+
+
+
+
+});
